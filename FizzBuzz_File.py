5d90cfde57729a6dbaec1172fb7e579b672110f3
xl3619-maker <xl3619@columbia.edu>
2026-03-01 22:16:47 -0500

    Update FizzBuzz_File.py

--- FizzBuzz_File.py
+++ FizzBuzz_File.py
@@ -5,9 +5,7 @@ def FizzBuzz(start, finish):
     out = nums.astype(object)
     mask3 = nums % 3 == 0
     mask5 = nums % 5 == 0
-
     out[mask3 & mask5] = "fizzbuzz"
     out[mask3 & ~mask5] = "fizz"
     out[mask5 & ~mask3] = "buzz"
-
     return out
