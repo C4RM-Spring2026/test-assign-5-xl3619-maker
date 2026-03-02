a0fd90e585aae6d9286fa0adcf0de42585cb7022
xl3619-maker <xl3619@columbia.edu>
2026-03-01 21:54:59 -0500

    Create BondPrice_File.py

---
+++ BondPrice_File.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def getBondPrice(y, face, couponRate, m, ppy=1):
+    n = int(m * ppy)              
+    r = y / ppy                   
+    c = face * couponRate / ppy   
+    t = np.arange(1, n + 1, dtype=float)     
+    df = (1.0 + r) ** (-t)                   
+    return c * df.sum() + face * df[-1]
