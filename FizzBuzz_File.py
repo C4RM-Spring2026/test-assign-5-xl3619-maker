43ec51e344d2529afbb14535067aa3b91f2561ab
xl3619-maker <xl3619@columbia.edu>
2026-03-01 22:15:22 -0500

    Update FizzBuzz_File.py

--- FizzBuzz_File.py
+++ FizzBuzz_File.py
@@ -1,14 +1,13 @@
 import numpy as np
 
-def FizzBuzz(a, b):
-    x = np.arange(a, b + 1)
-    out = x.astype(object)
+def FizzBuzz(start, finish):
+    nums = np.arange(start, finish+1)
+    out = nums.astype(object)
+    mask3 = nums % 3 == 0
+    mask5 = nums % 5 == 0
 
-    m3 = (x % 3 == 0)
-    m5 = (x % 5 == 0)
+    out[mask3 & mask5] = "fizzbuzz"
+    out[mask3 & ~mask5] = "fizz"
+    out[mask5 & ~mask3] = "buzz"
 
-    out[m3] = "fizz"
-    out[m5] = "buzz"
-    out[m3 & m5] = "fizzbuzz"
-
-    return out.tolist()
+    return out
